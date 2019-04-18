c31967c4398be5d24969973eceb718573aeeeeff
fr4nt1x <fr4nt1x>
2019-04-18 22:27:02 +0200

    added comments and first try with multiple implementation

--- main/main.go
+++ main/main.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
-	a := []float64{1, 2, 3, 4}
-	t := tensor.New(a, 2, 2)
+	a := []float64{1, 2, 3, 4, 5, 6, 7, 8}
+	t := tensor.New(a, 2, 2, 2)
 	fmt.Println(t)
-	for i := uint(0); i < 1; i++ {
-		for j := uint(0); j < 1; j++ {
-			fmt.Printf("T[%d,%d] : %f", i, j, t.Get(i, j))
+	for i := uint(0); i <= 1; i++ {
+		for j := uint(0); j <= 1; j++ {
+			for k := uint(0); k <= 1; k++ {
+				fmt.Printf("T[%d,%d,%d] : %f\n", i, j, k, t.Get(i, j, k))
+			}
 		}
 	}
 }
--- tensor/tensor.go
+++ tensor/tensor.go
@@ -3,39 +3,56 @@ package tensor
 import (
 	"fmt"
 
-	"github.com/fr4nt1x/linAlg/main/mathutils"
+	"github.com/fr4nt1x/linAlg/mathutils"
 )
 
-type tensor struct {
+//Struct Datastruct for tensor type
+type Struct struct {
 	shape   []uint
 	entries []float64
 }
 
+//Tensor Interface methods for type tensor
 type Tensor interface {
-	Get() tensor
+	Get([]uint) float64
+	GetList([]uint) []float64
 }
 
-func New(entries []float64, shape ...uint) tensor {
+//New Creates new tensor
+//Removes any entries of shape that are equal to 1
+//Checks that number of entries matches the product of shape
+func New(entries []float64, shape ...uint) Struct {
 	if uint(len(entries)) != mathutils.Prod(shape) {
 		panic("Wrong amoung of input for given shape.")
 	}
 	newShape := processShape(shape)
-	t := tensor{newShape, entries}
+	t := Struct{newShape, entries}
 	return t
 }
 
-func (inputTensor tensor) Get(indices ...uint) float64 {
+func processShape(shape []uint) []uint {
+	var newShape []uint
+	for _, i := range shape {
+		if i != 1 {
+			newShape = append(newShape, i)
+		}
+	}
+	return newShape
+}
+
+//Get returns the value of the given index
+func (inputTensor Struct) Get(indices []uint) float64 {
 	checkIndicesInRange(inputTensor, indices)
 	vectorizedIndex := uint(0)
-	lastIndex := len(indices)
-	for i, index := range indices[:lastIndex-1] {
+	lastIndex := len(indices) - 1
+	for i, index := range indices[:lastIndex] {
 		vectorizedIndex += index * mathutils.Prod((inputTensor.shape[i+1:]))
 	}
 	vectorizedIndex += indices[lastIndex]
 	return inputTensor.entries[vectorizedIndex]
 }
 
-func checkIndicesInRange(inputTensor tensor, indices []uint) {
+func checkIndicesInRange(inputTensor Struct, indices []uint) {
 	if len(indices) != len(inputTensor.shape) {
 		panic("Not enough indices given for tensor.")
 	}
@@ -46,13 +63,4 @@ func checkIndicesInRange(inputTensor tensor, indices []uint) {
 
 	}
 
-}
-func processShape(shape []uint) []uint {
-	var newShape []uint
-	for _, i := range shape {
-		if i != 1 {
-			newShape = append(newShape, i)
-		}
-	}
-	return newShape
 }
